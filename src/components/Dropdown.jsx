5591a437f4ac7ae3edc5c32716582ed0840d7bbb
Pablets <godoypablom@gmail.com>
2021-04-12 04:56:10 -0300

    Added ref to the height of the menu content

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -6,21 +6,23 @@ import { Frame } from 'framer'
 // import { list } from 'postcss'
 
 const Dropdown = ({ icon, label, children }) => {
-//   const isDesktop = useMediaQuery({
-//     query: '(min-device-width: 1224px)',
-//   })
+  //   const isDesktop = useMediaQuery({
+  //     query: '(min-device-width: 1224px)',
+  //   })
   const [scaleY, cycle] = useCycle(0, 1)
 
-  const listRef = React.useRef()
 
-  React.useEffect(() => {
-    console.log(listRef.current.clientHeight)
-  })
+//   const listRef = React.useRef()
+
+//   React.useEffect(() => {
+//     // console.log(listRef.current.clientHeight)
+//     setMenuHeight(listRef.current.clientHeight)
+//   }, [])
 
   //   onMouseLeave={() => scaleY === 1 && cycle()}
   return (
     <div>
-      <div>
+      <div className='mr-52'>
         <button
           className='text-decoration-none focus:outline-none p-2 leading-10 h-10 flex bg-red-500 rounded-sm'
           onClick={() => cycle()}>
@@ -37,17 +39,21 @@ const Dropdown = ({ icon, label, children }) => {
               d='M4 6h16M4 12h16M4 18h16'
             />
           </svg>
-          <span className='ml-2 font-sans font-normal text-xl leading-6 align-middle'>
-            {label}
+          <span className='font-sans font-normal text-xl leading-6 align-middle'>
+            {label && label}
           </span>
         </button>
         <Frame
+        className='relative'
           background={null}
           initial={{ scaleY: 0 }}
           originY={0}
           originX={0}
           animate={{ scaleY: scaleY, opacity: scaleY }}>
-          <div ref={listRef} className='bg-yellow-600 pb-0 p-3 mb-0'>{children}</div>
+          <div
+            className='bg-yellow-600 w-32 overflow-hidden absolute right-40 rounded-md rounded-r-none'>
+            {children}
+          </div>
         </Frame>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 import useRootClose from 'react-overlays/useRootClose'
 import Dropdown from './Dropdown'
 // import { FaUser } from 'react-icons/fa';
-// import { motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Header = () => {
   //   const isDesktop = useMediaQuery({
@@ -13,40 +13,80 @@ const Header = () => {
 
   const ref = useRef()
   const [show, setShow] = useState(false)
+  const [state, setState] = useState(true)
   const handleRootClose = () => setShow(false)
 
   useRootClose(ref, handleRootClose, {
     disabled: !show,
   })
 
-  //   const showItemCount = ''
-  //   const cartItems = ''
-  //   const userInfo = ''
+  const div1Ref = React.useRef()
+
+  const div2Ref = React.useRef()
+
+  const [menuHeight, setMenuHeight] = React.useState(null)
+//   const [menuWidth, setMenuWidth] = React.useState(null)
+
+//   console.log(menuHeight)
+
+  React.useEffect(() => {
+    if (!menuHeight || menuHeight !== div1Ref.current.clientHeight) {
+      setMenuHeight(div1Ref.current.clientHeight)
+    } else if (!menuHeight || menuHeight !== div2Ref.current.clientHeight) {
+      setMenuHeight(div2Ref.current.clientHeight)
+    }
+  }, [state])
 
   return (
-    <header className='flex px-4 justify-between'>
+    <header className='flex justify-between'>
       <h1 className='text-5xl font-bold text-gray-600'>
         Age of empires civilizations
       </h1>
-      <Dropdown icon={'fas fa-user'} label={'Just a label'}>
-        <div>
-          <NavLink to='/home' activeClassName='text-white'>
-            <button className='ml-7 focus:outline-none'>
-              <span className='font-sans font-normal text-base'>Home</span>
-            </button>
-          </NavLink>
-          <NavLink to='/autocomplete' activeClassName='text-white'>
-            <button className='ml-7 focus:outline-none'>
-              <span className='font-sans font-normal text-base'>
-                Autocomplete
-              </span>
-            </button>
-          </NavLink>
-          <button onClick={console.log('go right')}>Go right</button>
-          <h1>otro item</h1>
-          <h1>otro item</h1>
-          <h1>otro item</h1>
-        </div>
+      <Dropdown icon={'fas fa-user'} label={null}>
+        <motion.div
+          style={{ height: menuHeight }}
+          className={`transition-all duration-500 w-72 ${
+            !state
+              ? 'transform-gpu -translate-x-36'
+              : 'transform-gpu translate-x-0'
+          }`}>
+          <div
+          style={{ height: menuHeight }}
+          className='flex justify-evenly'>
+            <div ref={div1Ref} className='p-4 h-28 w-36'>
+              <button
+                onClick={() => {
+                  setState(!state)
+                  setMenuHeight(div1Ref.current.clientHeight)
+                }}>
+                Go right
+              </button>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+            </div>
+            <div ref={div2Ref} className='p-4 h-80 w-36'>
+              <button
+                onClick={() => {
+                  setState(!state)
+                  setMenuHeight(div2Ref.current.clientHeight)
+                }}>
+                Go left
+              </button>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+              <h1>otro item</h1>
+            </div>
+          </div>
+        </motion.div>
       </Dropdown>
     </header>
   )
